733f2d6c88433c950f35b403705c0afb6b36d2c7
Iordanis Paschalidis <iordanispaschalidis@Iordaniss-Mac-mini.local>
2024-01-17 12:14:01 +0000

    changing data structure for tdatra

--- models.go
+++ models.go
@@ -11,11 +11,11 @@ type FixtureByDateRequest struct {
 }
 
 type ArticleResponse struct {
-	Title    string      `json:"title"`
-	Body     string      `json:"body"`
-	HashTags []string    `json:"hash_tags"`
-	Type     string      `json:"type"`
-	Data     interface{} `json:"data"`
+	Title    string               `json:"title"`
+	Body     string               `json:"body"`
+	HashTags []string             `json:"hash_tags"`
+	Type     string               `json:"type"`
+	Data     FixtureDataStructure `json:"data"`
 }
 
 type FixtureByDateResponse struct {
@@ -26,4 +26,26 @@ type LeagueFixtures struct {
 	LeagueID   int    `json:"league_id"`
 	LeagueName string `json:"league_name"`
 	IDs        []int  `json:"ids"`
+}
+
+type FixtureDataStructure struct {
+	League                 string `json:"league"`
+	TeamOne                string `json:"team_one"`
+	TeamTwo                string `json:"team_two"`
+	TeamOneWins            int    `json:"team_one_wins"`
+	TeamTwoWins            int    `json:"team_two_wins"`
+	TeamOneDraws           int    `json:"team_one_draws"`
+	TeamTwoDraws           int    `json:"team_two_draws"`
+	TeamOneLoses           int    `json:"team_one_loses"`
+	TeamTwoLoses           int    `json:"team_two_loses"`
+	TeamOneGoals           int    `json:"team_one_goals"`
+	TeamTwoGoals           int    `json:"team_two_goals"`
+	TeamOneGoalsAgainst    int    `json:"team_one_goals_against"`
+	TeamTwoGoalsAgainst    int    `json:"team_two_goals_against"`
+	TeamOneCleanSheets     int    `json:"team_one_clean_sheets"`
+	TeamTwoCleanSheets     int    `json:"team_two_clean_sheets"`
+	TeamOneFailedToScore   int    `json:"team_one_failed_to_score"`
+	TeamTwoFailedToScore   int    `json:"team_two_failed_to_score"`
+	TeamOnePenaltiesScored int    `json:"team_one_penalties_scored"`
+	TeamTwoPenaltiesScored int    `json:"team_two_penalties_scored"`
 }
